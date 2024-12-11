260704b26b192b201d4df28888e425531f3f3ca0
raccoon0329 <kevinkuma33@gmail.com>
2024-12-11 19:48:48 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main()
+{
+    int times = 7;
+    for(int i = 1 ; i <= times ; i++){
+        printf("%*d" ,times-i+1 ,i );
+        for(int j = 1 ; j < i ; j++ ){
+            printf(" %d" ,i );
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
