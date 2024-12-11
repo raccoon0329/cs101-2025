0f9afe82c6db65a0ffc4b2a8c81fead77ae133f4
raccoon0329 <kevinkuma33@gmail.com>
2024-12-11 18:13:08 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    for (int i = 1; i <= 81; i++) {
+        int x = (i - 1) / 9 + 1;
+        int y = (i - 1) % 9 + 1;
+        printf("%d*%d=%-2d\t", x, y, x * y);
+        if (y == 9) printf("\n");
+    }
+    return 0;
+}
