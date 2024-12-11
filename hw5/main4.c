9dcc1497c3f88231a6977f77c35dd919af29bc46
raccoon0329 <kevinkuma33@gmail.com>
2024-12-11 18:12:33 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    int i = 12345;
+    int thousands = (i / 1000) % 10;
+    int ones = i % 10;
+    int result = i - thousands * 1000 - ones + ones * 1000 + thousands;
+    printf("%d\n", result);
+    return 0;
+}
