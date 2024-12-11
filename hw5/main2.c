906c775726448d2176bb05c190b604b4cf5bfe0d
raccoon0329 <kevinkuma33@gmail.com>
2024-12-11 19:49:25 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+    double pi = 4.0f ;
+    int sign = -1;
+    for(int i = 1 ; ; i++){
+        pi += sign * 4.0 / (2 * i + 1);
+        sign = -sign;
+        if(round(pi*100000)/100000 == 3.14159){
+            printf("i = %i\n",i);
+            printf("x = %i\n",2 * i + 1);
+            break;
+        }
+    }
+    printf("%.5f\n",pi);
+    
+    return 0;
+}
